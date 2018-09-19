631ddae73f97afd0724a0d71caf22faf527511aa
Luyao Pan <lpan@linux.mt.local>
2018-09-19 16:27:55 -0700

    add server render html page

---
+++ server/tpl/normal.js
@@ -0,0 +1,27 @@
+module.exports = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Koa Server HTML</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" crossorigin="anonymous">
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" crossorigin="anonymous"></script>
+        </head>
+        <body>
+          <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1>Hi Luyao</h1>
+                    <p>This is Kevin</p>
+                </div>
+                <div class="col-md-4">
+                    <p>测试静态 HTML 页面</p>
+                </div>
+            </div>
+          </div>
+        </body>
+    </html>
+`
