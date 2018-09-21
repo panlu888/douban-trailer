9603af3506f881de9dcd6a4ad5838f328cb23dae
Luyao <panluyaousa@gmail.com>
2018-09-20 20:59:44 -0700

    add stuff

---
+++ parcel/es/app.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default App = ({ name }) => {
+  return <div>{name}</div>
+}
---
+++ parcel/es/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './app'
+
+class AppContainer extends React.Component {
+  state = {
+    name: 'Parcel 打包案例'
+  }
+
+  componentDidMount () {
+    setTimeout(() => this.setState({ name: 'parcel da baobao'}), 2000)
+  }
+
+  render () {
+    return <App name={this.state.name} />
+  }
+}
+
+render(
+  <AppContainer />,
+  document.getElementById('app')
+)
